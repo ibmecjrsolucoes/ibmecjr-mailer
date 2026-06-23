a12b7cf05e9111789b1c8e4ebb8a6bf193a4462d
Heitor65 <multheitor@gmail.com>
2026-06-23 17:44:19 -0300

    add: função enviar email

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -27,13 +34,41 @@ class ContatoForm(BaseModel):
     resultadoEsperado: str
 
 
+def enviar_email(form: ContatoForm):
+    smtp_user  = os.getenv("SMTP_USER")
+    smtp_pass  = os.getenv("SMTP_PASS")
+    dest_email = os.getenv("DEST_EMAIL")
 
+    msg = MIMEMultipart()
+    msg["Subject"] = f"Novo Lead: {form.nome} — {form.empresa}"
+    msg["From"]    = smtp_user
+    msg["To"]      = dest_email
+    msg["Reply-To"] = form.email
 
+    corpo = f"""
+    Nome: {form.nome}
+    Email: {form.email}
+    Empresa: {form.empresa}
+    Cargo: {form.cargo}
+    Funcionários: {form.funcionarios}
+    Segmento: {form.segmento}
+    Serviços: {", ".join(form.servico)}
+    Desafio: {form.desafioPrincipal}
+    Prazo: {form.prazoSolucao}
+    Resultado esperado: {form.resultadoEsperado}
+    """
+
+    msg.attach(MIMEText(corpo, "plain"))
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(smtp_user, smtp_pass)
+        server.sendmail(smtp_user, dest_email, msg.as_string())
 
 
 
 
 @app.post("/contato")
-def receber_email():
-    
+def receber_email(form: ContatoForm):
+    enviar_email(form)
     return {"message":"Formulário enviado com sucesso!"}
