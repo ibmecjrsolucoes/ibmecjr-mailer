37bdb111c9c23c7af47f0875f9f0c7a94b67bc99
Heitor65 <multheitor@gmail.com>
2026-06-23 17:25:29 -0300

    add:contatoForm e configuração do CORS

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List, Optional
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://127.0.0.1:8000"],
+    allow_methods = ["POST", "OPTIONS"],
+    allow_headers = ["Content-Type"],
+)
 
+class ContatoForm(BaseModel):
+    nome: str
+    email: str
+    empresa: str
+    cargo: str
+    funcionarios: str
+    segmento: str
+    servico: List[str]
+    servicoOutro: Optional[str] = ""
+    desafioPrincipal: str
+    prazoSolucao: str
+    resultadoEsperado: str
 
 
 
